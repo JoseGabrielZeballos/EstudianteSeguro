68e5a2023004582834e80a0773f8a349f1f9ab2d
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2020-09-15 10:10:28 -0400

    unitTest

---
+++ ApiSeccureStudent/UtEstudianteSeguro.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ApiSeccureStudent.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtEstudianteSeguro.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetStudent()
+        {
+            //Arrange
+            StudentsController students = new StudentsController ();
+
+
+
+
+            //Act
+            var ListaEstudiante = students.GetStudents();
+
+
+            //Assert
+            Assert.IsNotNull(ListaEstudiante);
+
+
+        }
+    }
+}
